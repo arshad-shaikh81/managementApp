d60f9415b30eb2c3d47a2c89cbf372693c332f3e
arshad_shaikh81 <shekharshadulhak011@gmail.com>
2026-07-12 00:43:59 +0530

    Added all files

---
+++ src/main/java/org/managementapp/ManagementAppApplication.java
@@ -0,0 +1,13 @@
+package org.managementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/managementapp/ManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.managementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
