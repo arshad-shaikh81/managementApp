7fa5f8313b6200c41b148dc8db047161d32034fe
arshad_shaikh81 <shekharshadulhak011@gmail.com>
2026-07-21 14:45:13 +0530

    move all files

--- src/main/resources/static/frontend/All pages/ResidentPage/Dashboard/mainDashboard.js
+++ src/main/resources/static/frontend/All pages/ResidentPage/Dashboard/mainDashboard.js
@@ -40,7 +40,7 @@ document.getElementById('nav').addEventListener('click', function(e){
 });
 
 document.getElementById('logoutBtn').addEventListener('click', function(){
-    window.location.href = "../../Login_page/loginPage.html";
+    window.location.href = "../../RagistrationPages/Login_page/loginPage.html";
 });
 
 
@@ -71,7 +71,7 @@ userBtn.addEventListener('click', function(e){
 
 document.getElementById('dropdownLogout').addEventListener('click', function(e){
     e.preventDefault();
-    window.location.href = "../../Login_page/loginPage.html";
+    window.location.href = "../../RagistrationPages/Login_page/loginPage.html";
 });
 
 document.addEventListener('click', function(){
